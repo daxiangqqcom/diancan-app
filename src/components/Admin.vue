bf47142bb843c8535417e07ae128095e70f8538a
xiangbin <binxiang@maxxipoint>
2020-10-19 09:20:16 +0800

    阶段提交

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,3 +1,28 @@
 <template>
     <h1>Admin</h1>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                name: "henry"
+            }
+        },
+        beforeRouteEnter:(to,from,next)=> {
+            // alert("hello "+this.name);
+            // next();
+
+            next(vm=>{
+                alert('Hello '+vm.name);
+            })
+        },
+        beforeRouteLeave(to,from,next){
+            if(confirm("确认离开吗？")==true){
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,48 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import Home from './components/Home'
-import Menu from './components/Menu'
-import Admin from './components/Admin'
-import About from './components/about/About'
-import Login from './components/Login'
-import Register from './components/Register'
-
-// 二级路由
-import Contact from './components/about/Contact'
-import Delivery from './components/about/Delivery'
-import History from './components/about/History'
-import OrderingGuide from './components/about/OrderingGuide'
-
-// 三级路由
-import Phone from './components/about/contact/Phone'
-import PersonName from './components/about/contact/PersonName'
+import {routes} from './routes'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {path:'/',name:'homeLink',component:Home},
-  {path:'/menu',name:'menuLink',component:Menu},
-  {path:'/admin',name:'adminLink',component:Admin},
-  {path:'/about',name:'aboutLink',component:About,redirect:'/about/contact',
-    children:[
-      {path:'/about/contact',name:'contactLink',component:Contact,redirect:'/phone',
-      children:[
-          {path:'/phone',name:'phoneNumber',component:Phone},
-          {path:'/personname',name:'personName',component:PersonName}
-        ]},
-      {path:'/history',name:'historyLink',component:History},
-      {path:'/delivery',name:'deliveryLink',component:Delivery},
-      {path:'/orderingGuide',name:'orderingGuideLink',component:OrderingGuide}
-    ]},
-  {path:'/login',name:'loginLink',component:Login},
-  {path:'/register',name:'registerLink',component:Register},
-  {path:'*',redirect:'/'}
-]
-
 const router = new VueRouter({
   routes,
-  mode:'history'
+  mode:'history',
+  scrollBehavior(to,from,savedPosition){
+    // return {x:0,y:100}
+    // return {selector: '.btn'}
+
+    if(savedPosition){
+      return savedPosition
+    } else {
+      return {x:0,y:0}
+    }
+  }
+})
+
+// // 全局守卫
+// router.beforeEach((to,from,next) => {
+//   alert("还没登陆,请先登录!");
+//   next();
+//   console.log(from);
+//   console.log(to);
+
+//   // 判断store.gettes.isLogin === false
+//   if(to.path == '/login' || to.path == '/register') {
+//     next();
+//   } else {
+//     alert('还没登陆,请先登录!');
+//     next('/login');
+//   }
+// })
+
+router.afterEach((to,from)=>{
+  console.log();
 })
 
 new Vue({
